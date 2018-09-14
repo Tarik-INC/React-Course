cdbfd7cb304f9d476eb21c69cfdabe78ddccfa16
Tarik <tarikesmin@gmail.com>
2018-09-14 12:40:55 -0300

    Arquivos iniciais

---
+++ react-module1-assignment/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return(
+        <div> 
+            <p1> Usuário: {props.userName} </p1>
+            <p1> Espero também ser reescrito </p1>
+        </div>
+    );
+}
+
+export default userOutput;
