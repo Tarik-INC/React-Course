a7286ff04c7927e110a7af80da70a939781c40c3
Tarik <tarikesmin@gmail.com>
2018-09-16 01:26:18 -0300

    Modificado o arquivo react-module2 para preparar a adiçào de css modules no projeto

--- react-module2/src/App.js
+++ react-module2/src/App.js
@@ -5,17 +5,17 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { id : 'fafa1', name: 'Max', age: 28 },
-      { id :'fafa21', name: 'Manu', age: 29 },
-      { id : 'fafa64', name: 'Stephanie', age: 26 }
+      { id: 'fafa1', name: 'Max', age: 28 },
+      { id: 'fafa21', name: 'Manu', age: 29 },
+      { id: 'fafa64', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  
+
   nameChangedHandler = (event, id) => {
-    
+
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -29,63 +29,77 @@ class App extends Component {
     const persons = [...this.state.persons];
     //new persons
     persons[personIndex] = person;
-    
-    this.setState({persons: persons})
+
+    this.setState({ persons: persons })
 
   }
 
-deletePersonHandler = (personIndex) => {
-  //const persons = this.state.persons.slice();id = 'fafa'
-  const persons = [...this.state.persons];
-  persons.splice(personIndex, 1);
-  this.setState({persons: persons});
-}
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();id = 'fafa'
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
 
   togglePerson = () => {
     const showingPersons = this.state.showPersons
-    this.setState({showPersons: !(showingPersons)})
+    this.setState({ showPersons: !(showingPersons) })
   }
 
-  render () {
+  render() {
 
     const style = {
-      backgroundColor: 'White',
+      backgroundColor: 'Green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      cursor: 'pointer'
-    } 
+      cursor: 'pointer',
+    }
 
     let persons = null;
 
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
-      <div>
-        {this.state.persons.map( (person,index) => {
-          return <Person 
-          click={() => this.deletePersonHandler(index)}
-          name={person.name}
-          age={person.age}
-          changed={(evenet) => this.nameChangedHandler(evenet, person.id)}
-          key={person.id}
-          /> 
-        })}
-      </div>
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              changed={(evenet) => this.nameChangedHandler(evenet, person.id)}
+              key={person.id}
+            />
+          })}
+        </div>
       );
+      
+      style.backgroundColor = 'Red';
+     
     }
-    
+
+    const classes = [];
+
+    if(this.state.persons.length <= 2) {
+      classes.push('red'); // classes= ['red']
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
     return (
+      
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-        style={style}
-        onClick={this.togglePerson}>Switch Name</button> 
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePerson}>Switch Name</button>
         {persons}
       </div>
+    
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
 export default App;
-  
